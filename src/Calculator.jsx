e3e6d22c53962ab85549dce32625ee903e06d57b
iklochk0 <klochko.illya2002@gmail.com>
2023-07-26 21:17:01 +0200

    changed the placement of the buttons

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -28,10 +28,14 @@ const Calculator = () => {
     <div className="calculator">
       <div className="screen">{input}</div>
       <div className="buttons">
+        <button onClick={handleClear}>C</button>
+        <button onClick={handleDelete}>DEL</button>
+        <button style={{background:'#45a049'}}></button>
+        <button onClick={() => handleButtonClick('/')}>/</button>
         <button onClick={() => handleButtonClick('1')}>1</button>
         <button onClick={() => handleButtonClick('2')}>2</button>
         <button onClick={() => handleButtonClick('3')}>3</button>
-        <button onClick={() => handleButtonClick('+')}>+</button>
+        <button onClick={() => handleButtonClick('*')}>*</button>
         <button onClick={() => handleButtonClick('4')}>4</button>
         <button onClick={() => handleButtonClick('5')}>5</button>
         <button onClick={() => handleButtonClick('6')}>6</button>
@@ -39,13 +43,10 @@ const Calculator = () => {
         <button onClick={() => handleButtonClick('7')}>7</button>
         <button onClick={() => handleButtonClick('8')}>8</button>
         <button onClick={() => handleButtonClick('9')}>9</button>
-        <button onClick={() => handleButtonClick('*')}>*</button>
+        <button onClick={() => handleButtonClick('+')}>+</button>
+        <button onClick={() => handleButtonClick('^')}>^</button>
         <button onClick={() => handleButtonClick('0')}>0</button>
         <button onClick={() => handleButtonClick('.')}>.</button>
-        <button onClick={() => handleButtonClick('/')}>/</button>
-        <button onClick={handleDelete}>DEL</button>
-        <button onClick={handleClear}>C</button>
-        <button onClick={() => handleButtonClick('^')}>^</button>
         <button onClick={handleCalculate}>=</button>
       </div>
     </div>
